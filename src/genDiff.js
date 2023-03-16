9ecbb255800ed85781592b362ddfdfc98547e64e
JustGitHubUser9292 <notskillity@gmail.com>
2023-03-16 09:51:33 +0300

    upd

--- src/genDiff.js
+++ src/genDiff.js
@@ -49,8 +49,8 @@ return diffStr
 
 export default (filepath1, filepath2) => {
 
-const data1 = readFileSync(filepath1, 'utf-8')
-const data2 = readFileSync(filepath2, 'utf-8')
+const data1 = readFileSync(filepath1)
+const data2 = readFileSync(filepath2)
 
 const dataParse1 = JSON.parse(data1)
 const dataParse2 = JSON.parse(data2)
