6a2447fc9e741f6924ca8c73e228752a0dd0473b
JustGitHubUser9292 <notskillity@gmail.com>
2023-03-16 14:06:09 +0300

    upd

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -11,8 +11,14 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
+const expected = "{\n  - follow: false\n  host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}"
+
 test('json files genDiff', () => {
-  const expected = "{\n  - follow: false\n  host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}"
   expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(expected);
 });
+
+test('yaml files genDiff', () => {
+  expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'))).toEqual(expected);
+});
+
 
