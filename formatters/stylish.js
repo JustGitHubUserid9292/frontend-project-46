3e28ebe02daa89fe8ad065c3acc7fb74245e6890
JustGitHubUser9292 <notskillity@gmail.com>
2023-03-28 12:16:01 +0300

    upd

--- formatters/stylish.js
+++ formatters/stylish.js
@@ -2,13 +2,7 @@ import _ from 'lodash';
 
 const sortData = (data) => {
     const sortedKeys = Object.keys(data).sort((a, b) => {
-      if (a[0] === '+' || a[0] === '-') {
-        a = a.slice(2);
-      }
-      if (b[0] === '+' || b[0] === '-') {
-        b = b.slice(2);
-      }
-      return a.localeCompare(b);
+      return a.slice(2).localeCompare(b.slice(2));
     });
     const sortedObj = {};
     sortedKeys.forEach(key => {
@@ -24,12 +18,12 @@ const sortData = (data) => {
        } else if (!_.has(data1, key)) {
          acc[`+ ${key}`] = data2[key];
        } else if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
-         acc[key] = stylish(data1[key], data2[key]);
+         acc[`  ${key}`] = stylish(data1[key], data2[key]);
        } else if (!_.isEqual(data1[key], data2[key])) {
          acc[`- ${key}`] = data1[key];
          acc[`+ ${key}`] = data2[key];
        } else {
-         acc[key] = data1[key];
+         acc[`  ${key}`] = data1[key];
        }
        return sortData(acc);
      }, {});
